5b47b1bcf8b8690f68c86cb47037aa73bc94be0c
Andrey Mazurov <mazurovandre@gmail.com>
2023-05-30 17:18:15 +0300

    Added parallax

---
+++ parallax.js
@@ -0,0 +1,7 @@
+import Parallax from 'parallax-js'
+
+const scene = document.getElementById('scene');
+const parallaxInstance = new Parallax(scene);
+
+console.log(scene)
+console.log(parallaxInstance)
